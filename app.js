ad84bfa95e989d94ab0828cd74c6bf1dcb5f40eb
hannadiiahanesian <59712525+hannadiiahanesian@users.noreply.github.com>
2020-02-15 10:59:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://root:root123@cluster0-jef2u.azure.mongodb.net/test?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
